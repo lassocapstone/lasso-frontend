0e7c716942a0e01e177851409c3506eb536c9a6a
Samuel Kim <samjkim455@gmail.com>
2025-07-15 12:55:26 -0400

    added new Tasks and TasksExpanded components and also updated routes in App

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { Route, Routes } from "react-router";
 import Layout from "./layout/Layout";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
+import Tasks from "./Tasks";
+import Alerts from "./Alerts";
+import ProfileInfo from "./Profile";
 
 export default function App() {
   return (
@@ -10,6 +13,9 @@ export default function App() {
         <Route index element={<p>Home page</p>} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/alerts" element={<Alerts />} />
+        <Route path="/profile" element={<ProfileInfo />} />
       </Route>
     </Routes>
   );
