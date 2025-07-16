a873010e60ac9930a7f8c7c53a5efec76df960ae
Samuel Kim <samjkim455@gmail.com>
2025-07-15 20:37:02 -0400

    added pickaccount route in app, new component to pick accounttype, login will decide what page to navigate to based on if there is an accounttype for the user

---
+++ src/PickAccountType.jsx
@@ -0,0 +1,29 @@
+import useMutation from "./api/useMutation";
+import { useNavigate } from 'react-router';
+
+const PickAccountType = () => {
+  const { mutate: add } = useMutation("PUT", "/users", ["user"]);
+  //should be put or patch request?
+  const navigate = useNavigate();
+
+  const addAccountType = (formData) => {
+    const accountType = formData.get("accountType");
+    add({ accountType });
+    navigate('/');
+    //update navigate route to home page of whichever accounttype
+  };
+
+  return (
+    <>
+      <h1>Pick an Account Type</h1>
+      <form action={addAccountType}>
+        <label className='radio'> <input type="radio" name="accountType" value="organizer" /> Organizer </label>
+        <label className='radio'> <input type="radio" name="accountType" value="manager" /> Manager</label>
+        <label className='radio'> <input type="radio" name="accountType" value="subordinate" /> Subordinate</label>
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  )
+}
+
+export default PickAccountType;
