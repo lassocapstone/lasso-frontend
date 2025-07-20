9e384fdcc0bea39185ceae2e5dd9a6be5914f315
Bryan Cotto <brycotto1@yahoo.com>
2025-07-20 15:11:08 -0400

    Add homepage folder and modify homepage views

---
+++ src/homepage/AccountHomePage.jsx
@@ -0,0 +1,31 @@
+//use actual event name when the endpoint is available
+import useQuery from "../api/useQuery";
+import ManagerView from "../EventDetails/ManagerView";
+import Tasks from "../Tasks/Tasks";
+import OrganizerHome from "./OrganizerHomePage";
+
+const AccountHomePage = () => {
+  const { data: userData } = useQuery("/users", "user");
+  const { data: eventsData } = useQuery("/events", "event");
+  if (!userData) return <div>Loading user...</div>;
+  if (!eventsData) return <div>Loading event...</div>;
+  return (
+    <>
+      {userData && userData.account_type === "org" ? (
+        <OrganizerHome />
+      ) : userData.account_type === "man" ? (
+        <ManagerView />
+      ) : userData.account_type === "sub" ? (
+        <>
+          <h1>Welcome Subordinate!</h1>
+          <h1>Current Event: {eventsData.name}</h1>
+          <Tasks />
+        </>
+      ) : (
+        <h1>You need an account type.</h1>
+      )}
+    </>
+  );
+};
+
+export default AccountHomePage;
