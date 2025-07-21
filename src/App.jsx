b5ef142503d851011d1f6bceb1d7a0a2ecec8734
TanerJackson <jacksontaner@gmail.com>
2025-07-20 20:31:10 -0500

    casing issues in app.jsx routes fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,17 @@ import { Route, Routes } from "react-router";
 import Layout from "./layout/Layout";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
-import Tasks from "./Tasks/Tasks";
+import Tasks from "./tasks/Tasks";
 import Alerts from "./alerts/Alerts";
 import AccountHomePage from "./homepage/AccountHomePage";
-import ProfileInfo from "./Profile/ProfileInfo";
+import ProfileInfo from "./profile/ProfileInfo";
 import PickAccountType from "./profile/PickAccountType";
 import Error404 from "./Error404";
 import { Roster } from "./rosters/Roster";
 import { RosterUserDetails } from "./rosters/RosterUserDetails";
-import EventDetails from "./eventDetails/EventDetails";
+import EventDetails from "./EventDetails/EventDetails";
 import { EventSettings } from "./EventDetails/EventSettings";
-import EventCreate from "./eventDetails/EventCreate";
+import EventCreate from "./EventDetails/EventCreate";
 import TaskCreation from "./tasks/TaskCreation";
 
 export default function App() {
@@ -27,10 +27,16 @@ export default function App() {
         <Route path="/events/createEvent" element={<EventCreate />} />
         <Route path="/events/:eventId" element={<EventDetails />} />
         <Route path="/events/:eventId/tasks" element={<Tasks />} />
-        <Route path="/events/:eventId/tasks/createTask" element ={<TaskCreation />} />
+        <Route
+          path="/events/:eventId/tasks/createTask"
+          element={<TaskCreation />}
+        />
         <Route path="/events/:eventId/alerts" element={<Alerts />} />
         <Route path="/events/:eventId/roster" element={<Roster />} />
-        <Route path="/events/:eventId/roster/:userId" element={<RosterUserDetails />} />
+        <Route
+          path="/events/:eventId/roster/:userId"
+          element={<RosterUserDetails />}
+        />
         <Route path="/events/:eventId/settings" element={<EventSettings />} />
         <Route path="/*" element={<Error404 />} />
       </Route>
